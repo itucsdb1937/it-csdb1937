3ccd56751a703d98b3e7982e97c12600744fae01
emreislyn <isleyen16@itu.edu.tr>
2019-12-05 23:34:52 +0200

    host is added

--- server.py
+++ server.py
@@ -40,5 +40,6 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    port = app.config.get("PORT", 5000)
-    app.run(host="0.0.0.0", port=port)
+    #port = app.config.get("PORT", 5000)
+    app.run()
+    #host="0.0.0.0", port=port
